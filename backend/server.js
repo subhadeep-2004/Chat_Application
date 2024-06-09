bc14bf432c760b27bdfb43961cbb7987b7cfcc63
2205336 <2205336@kiit.ac.in>
2024-06-09 21:06:23 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -7,28 +7,32 @@ import messagesRoutes from './routes/message.routes.js'
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/userRoutes.routes.js'
 import { app,server } from './socket/socket.js';
+import path from "path"
 // git add .
 // git commit  -m "1st part completed-backend"
 
 
 // Mongodb details
 // username:bhadrasubhadeep2004
-//  pwd :Xiy2E4I7eq7CaoVc
-// connection_String :mongodb+srv://bhadrasubhadeep2004:Xiy2E4I7eq7CaoVc@cluster0.mzjlzw1.mongodb.net/chatAppDB?retryWrites=true&w=majority&appName=Cluster0
+
 dotenv.config()
 
 // app.use(bodyParser.urlencoded({extended:true}));
 // app.use("/api/auth",authRoutes)
 const PORT = process.env.PORT ||5000;
 
+const __dirname = path.resolve()
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello Wawdwdorlds")
-// })
+
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messagesRoutes)
 app.use("/api/users",userRoutes)
+app.use(express.static(path.join(__dirname, "/frontend/dist"    )))
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{
     connectDB()
